9d50c9230ce8a93d3c43636799f54e0dbb2af23a
pocari <caffelattenonsugar@gmail.com>
2019-11-10 15:55:02 +0900

    flutterのロゴが大きくなってくるように

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,15 +16,55 @@ class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            height: 300,
-            width: 300,
-            child: FlutterLogo(),
-          ),
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    print("----------------------------------------------------");
+    print("init state");
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this)
+          ..addListener(() {
+            print("----------------------------------------------------");
+            print("in lister");
+            setState(() {
+              print("----------------------------------------------------");
+              print("run setState");
+            });
+          });
+
+    animation = Tween<double>(
+      begin: 0,
+      end: 300,
+    ).animate(controller);
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    print("----------------------------------------------------");
+    print("dispose");
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("----------------------------------------------------");
+    print("build");
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          height: animation.value,
+          width: animation.value,
+          child: FlutterLogo(),
         ),
-      );
+      ),
+    );
+  }
 }
