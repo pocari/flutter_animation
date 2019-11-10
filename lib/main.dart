66dc08473472b9c16bff0cfe46b8cfb48ecb4d2b
pocari <caffelattenonsugar@gmail.com>
2019-11-10 15:32:17 +0900

    flutterのロゴを表示するだけ

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: null);
+    return MaterialApp(home: LogoApp());
   }
+}
+
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> {
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            height: 300,
+            width: 300,
+            child: FlutterLogo(),
+          ),
+        ),
+      );
 }
